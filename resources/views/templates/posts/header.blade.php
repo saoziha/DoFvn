2b55d2ed2b65881f340c2200d30d0eb22eb6651e
thanh1 <saoziha1997@gmail.com>
2019-11-29 10:59:53 +0700

    initial commit

--- resources/views/templates/posts/header.blade.php
+++ resources/views/templates/posts/header.blade.php
@@ -56,23 +56,6 @@
                 <!-- ============================================================== -->
                 <!-- Logo -->
                 <!-- ============================================================== -->
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="index.html">
-                        <!-- Logo icon -->
-                        <b>
-                            <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
-                            <!-- Dark Logo icon -->
-                            <img src="{{asset('templates/admin/assets/images/logo-icon.png')}}" alt="homepage" class="dark-logo" />
-
-                        </b>
-                        <!--End Logo icon -->
-                        <!-- Logo text -->
-                        <span>
-                            <!-- dark Logo text -->
-                            <img src="{{asset('templates/admin/assets/images/logo-text.png')}}" alt="homepage" class="dark-logo" />
-                        </span>
-                    </a>
-                </div>
                 <!-- ============================================================== -->
                 <!-- End Logo -->
                 <!-- ============================================================== -->
